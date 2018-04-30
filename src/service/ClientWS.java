9eb30648b8b105401edd85e1463072b2f5c38781
yassine iraqi <yassineiraqihoussaini@gmail.com>
2018-04-30 17:41:41 +0200

    first commit

---
+++ src/service/ClientWS.java
@@ -0,0 +1,10 @@
+package service;
+
+public class ClientWS {
+public static void main(String[] args) {
+	BanqueWS stub = new BanqueServiceService().getBanqueWSPort();
+	
+	double res = stub.convertionEuroToDh(800);
+	System.out.println(res);
+}
+}
